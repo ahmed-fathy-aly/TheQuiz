b0be46ed0988c5453aee211f4f49ec18adf2e32f
ahmed-fathy-forever-alone <unmei17@gmail.com>
2014-07-23 08:22:17 +0300

    added support library for gridlayout, changed home activity

--- src/asuspt/thequiz/activities/HomeActivity.java
+++ src/asuspt/thequiz/activities/HomeActivity.java
@@ -4,18 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 import asuspt.thequiz.R;
 
 /**
  * The home page
  * 
- * @author ahmed mohsen
  * 
  */
 public class HomeActivity extends Activity
 {
 
-	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
@@ -24,14 +24,20 @@ public class HomeActivity extends Activity
 	}
 
 	/**
-	 * Listener for the start activity button
+	 * opens the quiz activity
 	 */
-	public void startQuiz(View v)
+	public void startQuizActivity(View v)
 	{
-		// start quiz activity
-		Intent intent = new Intent(this, QuizActivity.class);
+		Intent intent = new Intent(HomeActivity.this, QuizActivity.class);
 		startActivity(intent);
-		finish();
+	}
+
+	/**
+	 * opens the profile activity
+	 */
+	public void startProfileActivity(View v)
+	{
+		// TODO start profile activity
 	}
 
 }
